c51ef219d9eaea6ddad8d27b4625692e7b79e709
0za0 <43097509+0za0@users.noreply.github.com>
2022-06-22 13:47:03 +0200

    Added Level Completion Editing

--- BionicleHeroesSaveEditor/BionicleHeroesSaveEditor/MainWindow.xaml.cs
+++ BionicleHeroesSaveEditor/BionicleHeroesSaveEditor/MainWindow.xaml.cs
@@ -2,18 +2,9 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BionicleHeroesSaveEditor
 {
@@ -23,10 +14,16 @@ namespace BionicleHeroesSaveEditor
     public partial class MainWindow : Window
     {
         private List<ToaConfig> AllToaConfigs;
+        private List<CheckBox> PirakaDoorsUnlocked;
+        private List<UIElementCollection> PirakaLevels;
         public MainWindow()
         {
             InitializeComponent();
             AllToaConfigs = new List<ToaConfig>() { JallerConfig, HahliConfig, KonguConfig, MatoroConfig, HewkiiConfig, NuparuConfig };
+            PirakaDoorsUnlocked = new List<CheckBox>() { UnlockHakann, UnlockVezok, UnlockAvak, UnlockThok, UnlockZaktan, UnlockReidak };
+            PirakaLevels = new List<UIElementCollection>() { HakannLevels.Children, VezokLevels.Children, AvakLevels.Children, ThokLevels.Children, ZaktanLevels.Children, ReidakLevels.Children };
+
+
             JallerConfig.ToaImage.Source = new BitmapImage(new Uri(@"/Images/ToaHeads/jaller.png", UriKind.Relative));
             KonguConfig.ToaImage.Source = new BitmapImage(new Uri(@"/Images/ToaHeads/kongu.png", UriKind.Relative));
             MatoroConfig.ToaImage.Source = new BitmapImage(new Uri(@"/Images/ToaHeads/matoro.png", UriKind.Relative));
@@ -49,6 +46,37 @@ namespace BionicleHeroesSaveEditor
             MoneySpentTextBox.Text = BitConverter.ToUInt32(Helpers.FileOperations.SaveDataBytes.GetRange(0xC8, 0xCB).ToArray()).ToString();
 
             FillToaTextBoxes();
+            FillLevelCheckBoxes();
+        }
+        private void FillLevelCheckBoxes()
+        {
+            //Other Hub Unlockables
+            int achievementsRoom = 0xAC;
+            int archesPosition = 0xAD;
+
+            AchievementDoorCheckbox.IsChecked = (Helpers.FileOperations.SaveDataBytes[achievementsRoom] != 0);
+            ArchesCheckbox.IsChecked = (Helpers.FileOperations.SaveDataBytes[archesPosition] != 0);
+
+            //Piraka Head Doors
+            int PirakaDoorStartLocation = 0x357;
+            for (int i = 0; i < 6; i++)
+            {
+                PirakaDoorsUnlocked[i].IsChecked = (Helpers.FileOperations.SaveDataBytes[PirakaDoorStartLocation] != 0);
+                PirakaDoorStartLocation++;
+            }
+
+            //Individual Levels
+            int individualLevelStartPos = 0x328;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    ((CheckBox)PirakaLevels[i][j]).IsChecked = (Helpers.FileOperations.SaveDataBytes[individualLevelStartPos] != 0);
+                    individualLevelStartPos++;
+                }
+            }
+            VezonOpenCheckBox.IsChecked = (Helpers.FileOperations.SaveDataBytes[0x35D] != 0);
+            VezonFinishedCheckBox.IsChecked = (Helpers.FileOperations.SaveDataBytes[individualLevelStartPos] != 0);
         }
 
         private void FillToaTextBoxes()
@@ -71,6 +99,7 @@ namespace BionicleHeroesSaveEditor
         }
         private void UpdateFile()
         {
+            //Toa Upgrades
             int healthStartLocation = 0x10;
             int upgradeStartLocation = 0x16;
             int weaponStartLocation = 0x1C;
@@ -86,6 +115,38 @@ namespace BionicleHeroesSaveEditor
                 upgradeStartLocation++;
                 weaponStartLocation++;
             }
+            //Level Completion
+            //Other Hub Unlockables
+            int achievementsRoom = 0xAC;
+            int archesPosition = 0xAD;
+
+            Helpers.FileOperations.SaveDataBytes[achievementsRoom] = (AchievementDoorCheckbox.IsChecked == true) ? (byte)01 : (byte)0;
+            Helpers.FileOperations.SaveDataBytes[archesPosition] = (ArchesCheckbox.IsChecked == true) ? (byte)01 : (byte)0;
+
+            //Piraka Head Doors
+            int PirakaDoorStartLocation = 0x357;
+            for (int i = 0; i < 6; i++)
+            {
+                Helpers.FileOperations.SaveDataBytes[PirakaDoorStartLocation] = (PirakaDoorsUnlocked[i].IsChecked == true) ? (byte)0x01 : (byte)0x0;
+                PirakaDoorStartLocation++;
+            }
+
+            //Individual Levels
+            int individualLevelStartPos = 0x328;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Helpers.FileOperations.SaveDataBytes[individualLevelStartPos] = (((CheckBox)PirakaLevels[i][j]).IsChecked == true) ? (byte)0x04 : (byte)0x00;
+                    individualLevelStartPos++;
+                }
+            }
+
+            //Big Bad ... Grr!
+            Helpers.FileOperations.SaveDataBytes[0x35D] = (VezonOpenCheckBox.IsChecked == true) ? (byte)0x01 : (byte)0x0;
+            Helpers.FileOperations.SaveDataBytes[individualLevelStartPos] = (VezonFinishedCheckBox.IsChecked == true) ? (byte)0x04 : (byte)0x0;
+            
+            //Money and Spent Money
             int newMoney = int.Parse(MoneyTextBox.Text);
             byte[] newMoneyBytes = BitConverter.GetBytes(newMoney);
             Helpers.FileOperations.SaveDataBytes[0xC4] = newMoneyBytes[0];
@@ -113,7 +174,7 @@ namespace BionicleHeroesSaveEditor
             //Helpers.FileOperations.SaveDataBytes[0xC5] = newMoneyBytes[1];
             //Helpers.FileOperations.SaveDataBytes[0xC6] = newMoneyBytes[2];
             //Helpers.FileReader.SaveDataBytes[0xC7] = newMoneyBytes[3];
-            
+
             UpdateFile();
             var newChecksum = Helpers.CheckSumFixer.FixCheckSum(Helpers.FileOperations.SaveDataBytes);
             CheckSumTextBox.Text = BitConverter.ToUInt32(newChecksum).ToString();
@@ -124,5 +185,7 @@ namespace BionicleHeroesSaveEditor
 
             Helpers.FileOperations.WriteNewSaveFile("./savegame.bin");
         }
+
+
     }
 }
